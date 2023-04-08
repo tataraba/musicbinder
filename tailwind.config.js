ba4dcc09cdbe7624261c2d64b0f7abaf8896bd06
Mario Munoz <pythonbynight@gmail.com>
2023-04-07 18:25:15 -0700

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,30 @@ module.exports = {
   content: ["./app/templates/**/*.html"],
   theme: {
     extend: {
+
+      /* Extend typography */
       fontFamily: {
         'livvic': ['Livvic', 'sans-serif'],
+      },
+
+      /* Extend available colors */
+      colors: {
+        primary: '#18956d',
+        secondary: '#3180b1',
+        turquoise: {
+          '50':  '#eef5f4',
+          '100': '#cdf0f2',
+          '200': '#95e6e1',
+          '300': '#5bcdbe',
+          '400': '#22af95',
+          '500': '#18956d',
+          '600': '#167e55',
+          '700': '#156144',
+          '800': '#104333',
+          '900': '#0c2927',
+        },
       }
     },
   },
   plugins: [],
 }
-
